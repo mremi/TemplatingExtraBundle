246dcea957db6130897dbb78d08d76648680f012
Rémi Marseille <marseille.remi@gmail.com>
2013-10-12 20:04:00 +0200

    Added copyrights

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Mremi\TemplatingExtraBundle Symfony bundle.
+ *
+ * (c) Rémi Marseille <marseille.remi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Mremi\TemplatingExtraBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
